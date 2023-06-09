4453b3171a8c9d9d5b37f3962afed4d933a48a8f
hiwina <hiwina@gmail.com>
2023-06-09 11:33:32 +0300

    Add page for Home Calculator and Quotes with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Quotes from './components/quotes';
+import Home from './components/Home';
+import Navbar from './components/Nav';
 
 const App = () => (
-  <div className="calculatorSection">
-    <Calculator />
-    <Quotes />
-  </div>
+  <>
+    <BrowserRouter>
+      <Navbar />
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quotes />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  </>
 );
 
 export default App;
